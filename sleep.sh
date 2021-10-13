a01f5357cdc5ace5b6bd4789067f3ea97fc271fe
Deepak Rai <92448078+Deepak-Rai883@users.noreply.github.com>
2021-10-13 20:46:36 +0530

    Add files via upload

---
+++ sleep.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+i=30
+for j in {1..31};do
+	if (($i >= 10)); 
+	then
+		echo "Screen will terminate in T-$i seconds..."
+		i=$((i-5))
+		j=$((j+5))
+		sleep 5;
+	else
+		echo "Screen will terminate in $i"
+         	i=$((i-1))
+		j=$((j+1)) 
+         	sleep 1;
+	fi
+	if (($i==0));then
+		break;
+	fi
+done
